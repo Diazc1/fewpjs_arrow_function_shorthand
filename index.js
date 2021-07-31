9aa4d9b1a2b04633c267f5269edbf716fcdf0cb2
Cathy Diaz <Cathydiaz14@gmail.com>
2021-07-31 00:48:04 -0700

    commit tree dbfb42ce643061b944b087b714eb3be9e3fcfd5d

---
+++ index.js
@@ -0,0 +1,16 @@
+// ✓ has a functional expression called divide
+// function divide() {
+// }
+
+
+// ✓ has a functional expression called divide
+// ✓ divide divides 2000 by 100
+let divide = () => 2000/100;
+
+
+// ✓ has an arrow function called square
+// ✓ square arrow function multiplies a number times itself
+let square = x => x * x;
+
+
+let add = (a, b) => a + b;
